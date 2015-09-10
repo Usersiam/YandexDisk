27a30d9121326913a2b80cb8d6b938a30c79405c
usersiam <gorbatovskij.roman@mail.ru>
2015-09-10 21:01:15 +0400

    записана инициализация нового класса, осталось дописать вывод по критериям

--- Лабораторная работа №1/ConsoleApplication1/ConsoleApplication1.cpp
+++ Лабораторная работа №1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,11 +1,25 @@
-// ConsoleApplication1.cpp: определяет точку входа для консольного приложения.
-//
-
-#include "stdafx.h"
-
+#include "stdafx.h" 
+#include "Abiturient.h"
+#include <iostream> 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	const int n = 4;
+	int a[n];
+	system("chcp 1251");
+	Abiturient a1, a2;
+	for (int i = 1; i > 5; i++)
+	{
+		std::cout << "введите  ";
+		std::cin >> a[i];
+	}
+	a1.setID_and_Marks(01,a[n]);
+	a1.setFamilia_Adress_Number("Пушкин", "Александр", "Сергеевич", "Ясная поляна 26", "002002002");
+	a2.setID_and_Marks(02, a[n]);
+	a2.setFamilia_Adress_Number("Демидов", "Александр", "Демидович", "Ясная поляна 26", "002002002");
+	//char* q = a1.getName();
+	//int w = a1.getAge();
+	a1.print();
+	a2.print();
 	return 0;
 }
-
