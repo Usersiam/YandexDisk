5af8ce1a43e2396dac5d3c11bb1dfe6cab103e93
usersiam <gorbatovskij.roman@mail.ru>
2015-09-13 23:41:19 +0300

    дописана проверка корректности записи баллов абитуриентов. вводить теперь можно только цифры от 1 до 999.
     дописаны 2 функции вывода данных по условию. Осталось дописать 3 вывод условия.

--- Лабораторная работа №1/ConsoleApplication1/ConsoleApplication1.cpp
+++ Лабораторная работа №1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,28 +4,31 @@
 using namespace std;
 int _tmain(int argc, _TCHAR* argv[])
 {
-	
 	system("chcp 1251");
 	Abiturient a[30];
 	int n;
 	cout << "введите количество абитуриентов :";
+	
 	cin >> n;
+	system("cls");
 	for (int i = 1; i < n+1; i++)
 	{
-		cout << i << " студент";
-		a[i].setID_and_Marks();
+		a[i].setID_and_Marks(i);
 		a[i].setFamilia_Adress_Number();
 		system("cls");
 
 	}
+
 	int b;
+	
 	for (;;)
 	{
-		cout << "---МЕНЮ---" << endl;
+		cout << endl<<endl;
+		cout<< "------------------------МЕНЮ--------------------------" << endl;
 		cout << "1___вывод всей информации" << endl;
-		cout << "2___вывод плохих отметок" << endl;
-		cout << "3___вывод хороших абитуриентов" << endl;
-		cout << "4___вывод абитуриентов, с суммой баллов выше заданной" << endl;
+		cout << "2___вывод плохих отметок"  << endl;
+		cout << "3___список абитуриентов, у которых сумма баллов выше заданной" << endl;
+		cout << "4___n абитуриентов, имеющих самую высокую сумму баллов" << endl;
 		cout << "5___выход" << endl;
 		cin >> b;
 		system("cls");
@@ -36,17 +39,57 @@ int _tmain(int argc, _TCHAR* argv[])
 			for (int i = 1; i < n+1; i++)
 			{
 				a[i].print();
+				cout << endl << "-----------------------------------";
 			}
 			
 			break;
 		case 2:
+			cout << "Задайте порог:" << endl;
+			int porog;
+			cin >> porog;
+			cout << "список абитуриентов, имеющих неудовлетворительные оценки: " << endl;
 			for (int i = 1; i < n+1; i++)
 			{
-				a[i].printBad();
+				a[i].printBad(porog);
+				cout << endl << "-----------------------------------";
 			}
 			
 			break;
-		
+		case 3:
+			cout << "Задайте сумму:" << endl;
+			int summa;
+			cin >> summa;
+			cout << "список абитуриентов, у которых сумма баллов выше заданной: " << endl;
+			for (int i = 1; i < n + 1; i++)
+			{
+				a[i].printSum(summa);
+				cout << endl << "-----------------------------------";
+			}
+
+			break;
+		case 4:
+			cout << "Задайте количество:" << endl;
+			int kolvo,b,max[30];
+			cin >> kolvo;
+			cout << "n абитуриентов, имеющих самую высокую сумму баллов: " << endl;
+			
+				for (int i = 0; i < n; i++)
+				{
+					max[i] = 0;
+
+					b = a[i].printGood(kolvo);
+					if (i = 1) max[i - 1] = 0;
+					if (b>max[i] && b<max[i-1]) max[i]=b;
+							
+			} 
+				for (int i = 1; i < kolvo+1; i++)
+				{
+					cout << max[i] << " ";
+				}
+				
+			break;
+		case 5:
+			break;
 		}
 	}
 	
