ae4cdf4aaa6af173da2060cf587e01165efc17e5
usersiam <gorbatovskij.roman@mail.ru>
2015-11-12 00:44:30 +0300

    7 и 8 лабараторная работа . 7 доделана. 8 начата.

--- Лабараторная работа №7 ( шаблон класса)/Лабараторная работа №7 ( шаблон класса)/Лабараторная работа №7 ( шаблон класса).cpp
+++ Лабараторная работа №7 ( шаблон класса)/Лабараторная работа №7 ( шаблон класса)/Лабараторная работа №7 ( шаблон класса).cpp
@@ -11,184 +11,116 @@ int main()
 	srand(time(NULL));
 	cout << "Размер матрицы (max=20)\n";
 	int m, n;
-	cin >> m;
-	cin >> n;
-
-	Matrix <kamen> third(m, n);
-	Matrix <kamen> forth(m, n);
-	Matrix< /*float*/ /*int*/  /*double*/ char  /*long*/	> first(m, n);
-	Matrix< /*float*/ /*int*/  /*double*/  char  /*long*/	> second(m, n);
+	cin >> m >> n;
+	Matrix <kamen,int> third(m, n);
+	Matrix <kamen, int> forth(m, n);
+	Matrix< double, int	> first(m, n);
+	Matrix< double, int	> second(m, n);
 	kamen a;
-	cout << "1- работы с обьектом, 2 - работа с числами\n";
+	
+	//7 лаба
+	cout << "1- работы с обьектом, 2 - работа с числами, 3 - шаблон функции \n";
 	int vib;
 	cin >> vib;
 	switch (vib)
 	{
 		//-------------------------  параметр - класс
 	case 1:
-	{
-		for (int i = 0; i < m; i++)
-			for (int j = 0; j < n; j++)
-			{
-				a.cost_kam = rand() % 100-50;
-				third.rand_new_obj(i, j, a);
-
-			}
-		for (int i = 0; i < m; i++)
-			for (int j = 0; j < n; j++)
-			{
-				a.cost_kam = rand() % 100-50;
-				forth.rand_new_obj(i, j, a);
-			}
+	{   
+		third.rand_new_obj();
+		forth.rand_new_obj();
 
 		system("cls");
 
-
 		cout << "Матрица 1\a\n";
-		for (int i = 0; i < m; i++)
-		{
-			for (int j = 0; j < n; j++)
-			{
-				cout << "|"; third.cout_new_obj(i, j, a); cout << "|";
+		third.cout_new_obj();
 
-
-			}cout << endl;
-		}
 		cout << "Матрица 2\a\a\n";
-		for (int i = 0; i < m; i++)
-		{
-			for (int j = 0; j < n; j++)
-			{
-				cout << "|"; forth.cout_new_obj(i, j, a); cout << "|";
+		forth.cout_new_obj();
 
-
-			}cout << endl;
-		}
 		
-
 		//----------------  сравнение
-		third -= forth;
+		
+		if (third -= forth) cout << "\n\nМатрицы равны по нулевому стобцу\n\n";
+		else cout << "\n\nМатрицы не равны по нулевому столбцу\n\n";
 		//------------------сумма
 		third / forth;
 		cout << "Сумма матриц\n";
-		for (int i = 0; i < m; i++)
-		{
-			for (int j = 0; j < n; j++)
-			{
-				cout << "|"; third.cout_new_obj(i, j, a); cout << "|";
-
-			}cout << endl;
-		}
-		
+		third.cout_new_obj();
 
 		//---------------- проверка на отрицательность
 
-		third [third];
-
+		third[third];
 
 		//==================== обнуление=======
-		
-		third = third;
-		cout << "Обнуление\n";
-		for (int i = 0; i < m; i++)
-		{
-			for (int j = 0; j < n; j++)
-			{
-				cout << "|"; third.cout_new_obj(i, j, a); cout << "|";
 
-			}cout << endl;
-		}
-		
+		third & third;
+		cout << "Обнуление\n";
+		third.cout_new_obj();
 		break;
 	}
-	
+
 	case 2:
 		//-------------------------  параметр - число
-		for (int i = 0; i < m; i++)
-		for (int j = 0; j < n; j++)
-			{
-				first.rand_new(i, j); 
-			}
-		for (int i = 0; i < m; i++)
-		for (int j = 0; j < n; j++)
-			{
-				 second.rand_new(i, j); 
-			}
-		
+		first.rand_new();
+		second.rand_new();
+		system("cls");
+
 		cout << "Матрица 1\a\n";
-		for (int i = 0; i < m; i++)
-		{for (int j = 0; j < n; j++)
-			{
-				 first.cout_new(i, j); 
+		first.cout_new();
 
-
-			}cout << endl;
-	}
 		cout << "Матрица 2\a\a\n";
-		for (int i = 0; i < m; i++)
-		{for (int j = 0; j < n; j++)
-			{
-				 second.cout_new(i, j); 
+		second.cout_new();
 
+		{if (first == second) cout << "\n\nМатрицы равны по нулевому стобцу\n\n";
+		else cout << "\n\nМатрицы не равны по нулевому столбцу\n\n";
 
-			}cout << endl;
-	}
-	{if (first == second) cout << "\n\nМатрицы равны по нулевому стобцу\n\n";
-	else cout << "\n\nМатрицы не равны по нулевому столбцу\n\n";
+		//===============  сумма =================================
 
+		first + second;
+		cout << "Сумма матриц\n";
+		first.cout_new();
 
-	//===============  сумма =================================
+		//==================== подсчет отрицательных членов=======
 
-	first + second;
-	cout << "Сумма матриц\n";
-	for (int i = 0; i < m; i++)
-	{
-		for (int j = 0; j < n; j++)
+		first%first;
+
+		////====================  обнуление ========================
+		cout << "Обнуление матрицы: 1 - первая, 2 - вторая\n";
+		int vib;
+		cin >> vib;
+		switch (vib)
 		{
-			first.cout_new(i, j);
-
-
-		}cout << endl;
-	}
-
-	
-
-	//==================== подсчет отрицательных членов=======
-
-	first(first);
-
-	////====================  обнуление ========================
-	cout << "Обнуление матрицы: 1 - первая, 2 - вторая\n";
-	int vib;
-	cin >> vib;
-	switch (vib)
-	{
-	case 1: first - first;
-		for (int i = 0; i < m; i++)
-		{
-			for (int j = 0; j < n; j++)
-			{
-				 first.cout_new(i, j); 
-
-
-			}cout << endl;
+		case 1: first - first;
+			first.cout_new();
+			break;
+		case 2: second - second;
+			second.cout_new();
+			break;
+		default: break;
 		}
-		break;
-	case 2: second - second;
-		for (int i = 0; i < m; i++)
-		{for (int j = 0; j < n; j++)
-			{
-				 second.cout_new(i, j);
+	case 3: 
+		/*Matrix <kamen, int> third(m, n);*/
+		Matrix < double,int > one_1(m, n);
+		//Matrix < int, int	> second_1(m, n);
+		one_1.rand_new();
+		one_1.cout_new();
+		cout << endl;
+		one_1.zero(0.3,one_1);
+		cout << endl;
 
-
-			}cout << endl;
+		/*second_1.rand_new();
+		second_1.cout_new();
+		cout << endl;
+		second_1.zero(2, second_1);
+		cout << endl;*/
+		/*third.rand_new_obj();
+		
+		third.cout_new_obj();
+		cout << endl<<endl;
+		third.zero((int)2,third);*/
+		
+		
+		}
 	}
-		break;
-	default: break;
-	}
-
-	}
-
-	}
-
 }
--- Лабараторная работа №7 ( шаблон класса)/Лабараторная работа №7 ( шаблон класса)/Матрица.h
+++ Лабараторная работа №7 ( шаблон класса)/Лабараторная работа №7 ( шаблон класса)/Матрица.h
@@ -1,128 +1,137 @@
 #include "stdafx.h"
 #include <iostream>
 using namespace std;
+
+
 class kamen
 {
 public:
 	int cost_kam;
-	kamen(){
+	kamen()
+	{
 	}
 };
-template <class T> 
+
+
+template <class T,class T2> 
 class Matrix
 {
 public:
-	T m[20][20];
+	T n[20][20];
+	T2 m[20][20];
 	int ch1, ch2;
 
-	Matrix(int m, int n)
+	Matrix(int z, int n)
 	{
-		ch1 = m;
+		ch1 = z;
 		ch2 = n;
 
 	}
+
 	
-	void rand_new(int i, int  j)
+	void rand_new()
 	{
-
-		m[i][j] = (rand() % 10 - 5) + 0.3*(i-j);
-
-
+		for (int i = 0; i < ch1; i++)
+			for (int j = 0; j < ch2; j++)
+			{
+				m[i][j] = (rand() % 10 - 5) + 0.3*(i - j);
+			}
 	}
-	void rand_new_obj(int i, int  j, kamen& obj)
+
+
+	void rand_new_obj()
 	{
-
-
-		m[i][j] = obj;
-
-	}
-	void cout_new(int i, int  j)
-	{
-		cout << m[i][j] << "  ";
-				
+		for (int i = 0; i < ch1; i++)
+			for (int j = 0; j < ch2; j++)
+			{
+				n[i][j].cost_kam = (rand() % 10 - 5) + 0.3*(i - j);
+			}
 			
 	}
 
-	void cout_new_obj(int i, int  j, kamen obj)
+
+	void cout_new()
 	{
-		
-		
-			obj = m[i][j];
-			if (obj.cost_kam >= -200)
+		for (int i = 0; i < ch1; i++)
+		{for (int j = 0; j < ch2; j++)
 			{
-				cout << obj.cost_kam;
-				
-			}
+				cout << "|"; cout << m[i][j] << "|";
+			}cout << endl;
+		}
+	}
+
+
+	void cout_new_obj()
+	{
+		for (int i = 0; i < ch1; i++)
+		 {for (int j = 0; j < ch2; j++)
+			{
+				cout << "|"; cout << n[i][j].cost_kam; cout << "|";
+			}cout << endl;
+		}
+		
 		
 		
 					
 	}
 	friend Matrix operator / (Matrix& a, Matrix& b)
 	{
-		kamen a1, b1, buf;
-
 		for (int i = 0; i < a.ch1; i++)
 			for (int j = 0; j <a.ch2; j++)
 			{
-				a1 = a.m[i][j];
-				b1 = b.m[i][j];
-				buf.cost_kam = a1.cost_kam + b1.cost_kam;
-				a.m[i][j] = buf;
+				
+				a.n[i][j].cost_kam = a.n[i][j].cost_kam + b.n[i][j].cost_kam;
+				
 			}
 		return a;
 	}
-	int operator = (Matrix& a)
+	void operator & (Matrix& a)
 	{
 		int buf = 0;
-		kamen a1;
 		for (int i = 0; i < a.ch1; i++)
 			for (int j = 0; j < a.ch2; j++)
 			{
-				a1 = a.m[i][j];
-
-				a1.cost_kam = buf;
-				a.m[i][j] = a1;
+				a.n[i][j].cost_kam= buf;
+				
 			}
-		return buf;
+		
 	}
 	friend bool operator -= (Matrix &a, Matrix &b)
 	{
 		int ch = 0;
-		kamen a1, b1;
-
+		cout << endl;
 		for (int i = 0; i < a.ch1; i++)
-		{
-			a1 = a.m[i][0];
-			b1 = b.m[i][0];
-			cout << a1.cost_kam << " " << b1.cost_kam << "\n";
-			if (a1.cost_kam == b1.cost_kam)
+		{	
+			cout << a.n[i][0].cost_kam << " " << b.n[i][0].cost_kam << "\n";
+			if (a.n[i][0].cost_kam == b.n[i][0].cost_kam)
 			{
 				
-				ch++; cout << "\n MAtrica 1 = Matrica 2\n";
+				ch++; cout << " MAtrica 1 = Matrica 2\n";
 			}
-			if (a1.cost_kam > b1.cost_kam)
+			if (a.n[i][0].cost_kam > b.n[i][0].cost_kam)
 			{
-				cout << "\n MAtrica 1 > Matrica 2\n";
+				cout << " MAtrica 1 > Matrica 2\n";
 
 			}
-			if (a1.cost_kam < b1.cost_kam)
+			if (a.n[i][0].cost_kam < b.n[i][0].cost_kam)
 			{
-				cout << "\n MAtrica 1 < Matrica 2\n";
+				cout << "MAtrica 1 < Matrica 2\n";
 			}
 		}
+		cout << endl;
 		if (ch == a.ch2) return true;
 
 		else  return false;
 	}
 	int operator [] (Matrix& a)
 	{
-		kamen a1;
+		
 		int buf = 0;
 		for (int i = 0; i < a.ch1; i++)
 			for (int j = 0; j < a.ch2; j++)
 			{
-				a1 = a.m[i][j];
-				if (a1.cost_kam<0) buf++;
+				
+				if (a.n[i][j].cost_kam <0) buf++;
 
 			}
 		cout << "Количество отрицательных элементов в матрице:\n" << buf << "\n";
@@ -159,28 +168,29 @@ public:
 	friend bool operator == (Matrix &a, Matrix &b)
 	{
 		T buf = 0, ch = 0;
-
+		cout << endl;
 		for (int i = 0; i < a.ch1; i++)
 		{
+			cout << a.m[i][0] << " " << b.m[i][0] << "\n";
 			if (a.m[i][0] == b.m[i][0])
 			{
-				ch++; cout << "\n MAtrica 1 = Matrica 2\n";
+				ch++; cout << "MAtrica 1 = Matrica 2\n";
 			}
 			if (a.m[i][0]>b.m[i][0])
 			{
-				cout << "\n MAtrica 1 > Matrica 2\n";
+				cout << "MAtrica 1 > Matrica 2\n";
 
 			}
 			if (a.m[i][0]<b.m[i][0])
 			{
-				cout << "\n MAtrica 1 < Matrica 2\n";
+				cout << "MAtrica 1 < Matrica 2\n";
 			}
 		}
 		if (ch == a.ch2) return true;
 
 		else  return false;
 	}
-	T operator() (Matrix& a)
+	T operator % (Matrix& a)
 	{
 		T buf = 0;
 		for (int i = 0; i < a.ch1; i++)
@@ -191,7 +201,42 @@ public:
 			}
 		cout << "Количество отрицательных элементов в матрице:\n" << buf << "\n";
 		return buf;
+	}	
+	void operator () (Matrix&  a)
+	{
+		
+		for (int i = 0; i < ch1; i++)
+		{
+			for (int j = 0; j < ch2; j++)
+			{
+
+				a.m[i][j] = a.n[i][j].cost_kam;
+			}
+		}
+	}
+
+
+
+
+	template <class T3>
+	void zero(T3 x,Matrix  a)
+	{
+		a(a);
+			
+		for (int i = 0; i < ch1; i++)
+			for (int j = 0; j < ch2; j++)
+			{
+				if (m[i][j] == x) m[i][j] = 0;
+			}
+		for (int i = 0; i < ch1; i++)
+		{
+			for (int j = 0; j < ch2; j++)
+			{
+				cout << "|"; cout << m[i][j]; cout << "|";
+			}cout << endl;
+		}
 	}
 };
+
 
 
